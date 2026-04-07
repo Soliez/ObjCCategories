c54bd9ed9189f8e6eb13d55238771a4df99d8950
Erik Solis <107826254+Soliez@users.noreply.github.com>
2026-04-06 22:14:57 -0400

    Initial Commit

---
+++ ObjCCategories/ObjCCategories.h
@@ -0,0 +1,12 @@
+//
+//  ObjCCategories.h
+//  ObjCCategories
+//
+//  Created by Erik Solis  on 2026-04-06.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ObjCCategories : NSObject
+
+@end
---
+++ ObjCCategories/ObjCCategories.m
@@ -0,0 +1,12 @@
+//
+//  ObjCCategories.m
+//  ObjCCategories
+//
+//  Created by Erik Solis  on 2026-04-06.
+//
+
+#import "ObjCCategories.h"
+
+@implementation ObjCCategories
+
+@end
